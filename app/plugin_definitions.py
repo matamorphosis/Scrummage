c2d0c4a460fe2134b5b3a8930d019eb2b62d5a6b
matamorphosis <mbrittain15@gmail.com>
2021-12-18 17:09:31 +1100

    Plugin Definition Restructure

--- app/plugin_definitions.py
+++ app/plugin_definitions.py
@@ -1,4 +1,5 @@
-import json
+import json, os
 
-JSON_File = open("./static/json/plugin_definitions.json", "r")
+File_Dir = os.path.dirname(os.path.realpath('__file__'))
+JSON_File = open(os.path.join(File_Dir, "static/json/plugin_definitions.json"), "r")
 Valid_Plugins = json.load(JSON_File)
